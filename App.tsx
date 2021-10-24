2149aac98673d991c4526bb20adf467d1450397b
Alex Profir <alex_profir@yahoo.com>
2021-10-25 00:36:29 +0300

    added webview

--- App.tsx
+++ App.tsx
@@ -1,63 +1,166 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Image, TextInput, Button, Alert, ToastAndroid, } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { StyleSheet, Text, View, Image, TextInput, Button, Alert, ToastAndroid, ImageProps, Platform, InteractionManager, NativeSyntheticEvent, ImageErrorEventData, } from 'react-native';
+import { WebView } from 'react-native-webview';
 import * as IntentLauncher from 'expo-intent-launcher';
-function validURL(str: string) {
-  const pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-    '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-    '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-    '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
-  return !!pattern.test(str);
+import * as Notifications from 'expo-notifications';
+import Constants from 'expo-constants';
+import * as Clipboard from 'expo-clipboard';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+async function registerForPushNotificationsAsync() {
+  let token = '';
+  if (Constants.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  return token;
 }
+
+async function schedulePushNotification() {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "You've got mail! 📬",
+      body: 'Here is the notification body',
+      data: { data: 'goes here' },
+    },
+    trigger: { seconds: 2 },
+  });
+}
+
+function AsyncImage(p: ImageProps) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<NativeSyntheticEvent<ImageErrorEventData>>(null!);
+  return <View>
+    <Image
+      onLoadStart={() => {
+        setLoading(true);
+      }}
+      onLoadEnd={() => {
+        setLoading(false);
+      }}
+      onError={(e) => {
+        console.log("ERROR");
+        // console.log(e);
+        setError(e);
+        setLoading(false);
+      }}
+      {...p}
+    />
+    <Text>
+      {loading && "Loading ..."}
+    </Text>
+    <Text>
+      {error && "Error loading the image"}
+    </Text>
+  </View>
+}
+
 export default function App() {
-  const [userString, setUserString] = useState("")
+  const [expoPushToken, setExpoPushToken] = useState('');
+
+  const [copiedText, setCopiedText] = useState('');
+  const [notification, setNotification] = useState<Notifications.Notification>(null!);
+  const notificationListener = useRef<any>();
+  const responseListener = useRef<any>();
+  // useEffect(() => {
+
+  //   registerForPushNotificationsAsync().then(token => setExpoPushToken(token!));
+
+  //   notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+  //     setNotification(notification);
+  //   });
+
+  //   responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+  //     console.log(response);
+  //   });
+
+
+  //   return () => {
+  //     Notifications.removeNotificationSubscription(notificationListener.current);
+  //     Notifications.removeNotificationSubscription(responseListener.current);
+  //   }
+  // }, [])
   return (
     <View style={styles.container}>
-      <Button title="Populate input" onPress={() => {
-        setUserString("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-      }} />
-      <TextInput
-        value={userString}
-        placeholder="Enter your name"
-        onChangeText={setUserString}
-        style={styles.textInput}
-      />
-      <View style={styles.buttonGroup}>
-        <Button title="Share" onPress={async () => {
-          try {
-            const result = await IntentLauncher.startActivityAsync("android.intent.action.SEND", {
-              extra: {
-                "android.intent.extra.TEXT": userString,
-              },
-              type: "text/plain",
-            })
+      <View style={{
+        marginTop: 50,
+        backgroundColor: "blue",
+        width: "100%",
+        height: "50%"
+      }}>
+        <WebView
+          source={{
+            uri: "https://www.google.com/imghp?hl=en",
+          }}
+        />
+      </View>
+      <View style={{
+        margin: 2,
+      }}>
+        <Button title="Load Image with background" onPress={async () => {
+          // await schedulePushNotification();
+          const text = await Clipboard.getStringAsync();
+          setCopiedText(text);
+        }} />
+      </View>
+      <View
+        style={{
+          margin: 2
+        }}
+      >
+        <Button title="Load Image with foreground" onPress={() => {
 
-            if (result.resultCode === IntentLauncher.ResultCode.Success) {
-              ToastAndroid.show("Success", 30);
-              console.log("success");
-            }
-          } catch (e) {
-            console.log({ e });
-          }
         }} />
-        <Button title="Search online" onPress={async () => {
-          try {
-            if (!validURL(userString)) {
-              ToastAndroid.showWithGravity("Invalid URL", 30, ToastAndroid.BOTTOM);
-              throw {};
-            }
-            const result = await IntentLauncher.startActivityAsync("android.intent.action.VIEW", {
-              data: userString
-            })
-            if (result.resultCode === IntentLauncher.ResultCode.Success) {
-              console.log("success");
-            }
-          } catch (e) {
-            console.log({ e });
-          }
+      </View>
+      <View
+        style={{
+          margin: 2
+        }}
+      >
+        <Button title="Set Clipboard" onPress={async () => {
+          const text = await Clipboard.getStringAsync();
+          setCopiedText(text);
         }} />
+      </View>
+      <View>
+        <Text>
+          {copiedText}
+        </Text>
+      </View>
+      <View>
+        {copiedText ? <AsyncImage style={{
+          width: 100,
+          height: 100
+        }} source={{ uri: copiedText }} /> : <View />}
       </View>
       <StatusBar style="auto" />
     </View>
@@ -66,10 +169,11 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    padding: 5,
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
 
   },
   btn: {
